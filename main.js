db373abd0a8eddd7f273fe015c9902a6fb67650e
Diogo <75549829+Diogo-ls@users.noreply.github.com>
2021-10-23 19:04:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'diogo-ls',
+  youtube: 'channel/UCeK1F4Tp5vci2eK8sTszgwQ',
+  facebook: 'diogoleal',
+  instagram: 'diogo_lleal',
+  twitter: 'diogoleal121'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
